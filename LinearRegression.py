4db58bccf1c24e50a553dd71098f6dc51941a9dd
DongYoung <billy009100@naver.com>
2018-09-28 16:48:05 +0900

    Initial commit

---
+++ LinearRegression.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+# X and Y data
+x_train = [1,2,3]
+y_train = [1,2,3]
+
+#x =1 일때 y = 1 x = 2일때 y = 2 와 같은 데이터.
+
+w = tf.Variable(tf.random_normal([1]),name = 'weight')
+# tensorflow가 자체적으로 변경시키는 변수 학습하는 과정에서 스스로 변경 [1] = 값을 1개를 사용한다는 것을 의미
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+# Our hypothesis XW+b
+hypothesis = x_train*w + b
+
+#cost/loss function
+cost = tf.reduce_mean(tf.square(hypothesis - y_train))
+#reduce_mean = 평균을 내주는 작업을 하는 메소드
+
+#Minimize 중요 !
+optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.01)
+train = optimizer.minimize(cost)
+#train -> 그래프의 이름 (node)
+
+#Launch the graph in a session
+sess = tf.Session()
+#Initializes global variables in the graph.
+sess.run(tf.global_variables_initializer())
+
+#Fit the Line
+for step in range(2001):
+    sess.run(train)
+    if step % 20 == 0:
+        print(step,sess.run(cost),sess.run(w) , sess.run(b))
+
---
+++ Placeholder.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+
+W = tf.Variable(tf.random_normal([1]), name='weight')
+b = tf.Variable(tf.random_normal([1]),name='bias')
+X = tf.placeholder(tf.float32 , shape = [None]) # 개수를 제한할 수 있음 shape를 사용하여.
+Y = tf.placeholder(tf.float32 , shape = [None])
+
+#Our hypothesis XW + b
+hypothesis = X * W + b
+
+# cost/loss function
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+
+# Minimize
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+train = optimizer.minimize(cost)
+
+# Launch the graph in a session
+sess = tf.Session()
+# Initializes global variables in the graph
+sess.run(tf.global_variables_initializer())
+
+# Fit the line with new training data
+for step in range(10000):
+    cost_val , W_val, b_val, _ =  sess.run([cost,W,b,train],
+    feed_dict={X: [1,2,3,4,5] , Y: [2.1,3.1,4.1,5.1,6.1]}) # 학습 데이터 ( X , Y )를 준다.
+
+
+    if step % 100 == 0:
+        print(step , cost_val , W_val , b_val)
+        #step / cost / w / b 값 순서대로 화면에 출력
+
+#Testing our model
+print(sess.run(hypothesis, feed_dict={X :[5]}))
+print(sess.run(hypothesis, feed_dict={X :[2.5]}))
+print(sess.run(hypothesis, feed_dict={X:[1.5,3.5]}))
